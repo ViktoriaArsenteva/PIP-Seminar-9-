09f28029f0fab1959828320e94b092b5d9c5b551
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-10-03 12:14:14 +0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pytube import YouTube
-
-video_link = 'https://www.youtube.com/watch?v=8ZpVwAeLzm4'
-yt = YouTube(video_link)
-videos = yt.videos
-videos.download()
+url="https://www.youtube.com/watch?v=n06H7OcPd-g"
+video = YouTube(url)
+video = video.streams.get_highest_resolution()
+video.download()
